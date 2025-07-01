70c3b303f403ad4f936c504278d192fe6b431ec2
hayden <hdn.c.smith@gmail.com>
2025-07-01 15:38:38 -0400

    Outline menu options

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello! Welcome to the Math Game!");
+        Console.WriteLine("--------------------------------------------------------------------------------------");
+        Console.WriteLine("Please select a menu option:");
+        Console.WriteLine("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. View Previous Games\n6. Exit");
     }
 }
