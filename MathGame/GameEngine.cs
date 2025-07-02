f6364d7be00959e1296089b1d84445a6ba7480ad
hayden <hdn.c.smith@gmail.com>
2025-07-01 22:24:40 -0400

    Complete Addition Game functionality

--- MathGame/GameEngine.cs
+++ MathGame/GameEngine.cs
@@ -6,25 +6,65 @@ internal class GameEngine
 {
     internal static void PlayAddition()
     {
-        Console.WriteLine("Addition Game coming soon. Press enter to continue.");
+        Random random = new Random();
+        int score = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Clear();
+            
+            int num1 = random.Next(1, 101);
+            int num2 = random.Next(1, 101);
+            int correctAnswer = num1 + num2;
+            
+            Console.WriteLine($"{num1} + {num2}");
+            
+            int userAnswer = GetUserAnswer();
+            if (userAnswer == correctAnswer)
+            {
+                Console.WriteLine("Correct! Press enter to continue.");
+                score++;
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, the correct answer was {correctAnswer}. Press enter to continue.");
+                Console.ReadLine();
+            }
+        }
+        Console.Clear();
+        Console.Write($"Your final score was {score}. Press enter to return to menu.");
         Console.ReadLine();
     }
-
+    
     internal static void PlaySubtraction()
     {
         Console.WriteLine("Subtraction Game coming soon. Press enter to continue.");
         Console.ReadLine();
     }
-
+    
     internal static void PlayMultiplication()
     {
         Console.WriteLine("Multiplication Game coming soon. Press enter to continue.");
         Console.ReadLine();
     }
-
+    
     internal static void PlayDivision()
     {
         Console.WriteLine("Division Game coming soon. Press enter to continue.");
         Console.ReadLine();
+    }
+
+    private static int GetUserAnswer()
+    {
+        string? input;
+        int userAnswer;
+        
+        do
+        {
+            input = Console.ReadLine();
+        } while (!int.TryParse(input, out userAnswer));
+        
+        return userAnswer;
+
     }
 }
