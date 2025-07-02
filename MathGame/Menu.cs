71585ba3266565703685c088dac53c09b2423a70
hayden <hdn.c.smith@gmail.com>
2025-07-01 21:39:36 -0400

    Extract menu functionality to new Menu class

---
+++ MathGame/Menu.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace MathGame;
+
+internal class Menu
+{
+    private int _menuSelection = 0;
+    internal int MenuSelection
+    {
+        get => _menuSelection;
+        set
+        {
+            if (value > 6)
+            {
+                _menuSelection = 0;
+            }
+            else
+            {
+                _menuSelection = value;
+            }
+        }
+    }
+    internal static void DisplayMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Hello! Welcome to the Math Game!");
+        Console.WriteLine("--------------------------------------------------------------------------------------");
+        Console.WriteLine("Please select a menu option:");
+        Console.WriteLine(
+            "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. View Previous Games\n6. Exit");
+    }
+    public static int GetMenuSelection()
+    {
+        string? input = Console.ReadLine();
+        if (int.TryParse(input, out int output))
+        {
+            return output;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static void PlaySelection(int menuSelection)
+    {
+        switch (menuSelection)
+        {
+            case 1:
+                GameEngine.PlayAddition();
+                break;
+            case 2:
+                GameEngine.PlaySubtraction();
+                break;
+            case 3:
+                GameEngine.PlayMultiplication();
+                break;
+            case 4:
+                GameEngine.PlayDivision();
+                break;
+            case 5:
+                Console.WriteLine("Display Previous Games option coming soon. Press enter to continue.");
+                Console.ReadLine();
+                // DisplayPreviousGames()
+                break;
+        }
+    }
+}
--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -6,39 +6,14 @@ class Program
 {
     static void Main()
     {
-        string? menuSelection = "";
+        Menu menu = new Menu();
         
-        while (menuSelection != "6")
+        while (menu.MenuSelection != 6)
         {
-            Console.Clear();
-            Console.WriteLine("Hello! Welcome to the Math Game!");
-            Console.WriteLine("--------------------------------------------------------------------------------------");
-            Console.WriteLine("Please select a menu option:");
-            Console.WriteLine(
-                "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. View Previous Games\n6. Exit");
-
-            menuSelection = Console.ReadLine();
-
-            switch (menuSelection)
-            {
-                case "1":
-                    GameEngine.PlayAddition();
-                    break;
-                case "2":
-                    GameEngine.PlaySubtraction();
-                    break;
-                case "3":
-                    GameEngine.PlayMultiplication();
-                    break;
-                case "4":
-                    GameEngine.PlayDivision();
-                    break;
-                case "5":
-                    Console.WriteLine("Display Previous Games option coming soon. Press enter to continue.");
-                    Console.ReadLine();
-                    // DisplayPreviousGames()
-                    break;
-            }
+            
+            Menu.DisplayMenu();
+            menu.MenuSelection = Menu.GetMenuSelection();
+            Menu.PlaySelection(menu.MenuSelection);
         }
     }
 }
